b102f067a5f57bc8ba1663c13e4c953ebcf8732d
Samuel Sukovský <145551742+SamuelSukovsky@users.noreply.github.com>
2024-02-02 21:27:07 +0000

    Comments, comments everywhere

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,50 +28,50 @@ public class App
 
     public void start() throws FileNotFoundException
     {
-        // parkingLot();
-        // floodFill();
-        // identifierCount("source.java");
-        // System.out.println(nestedTags("nestedTags.txt"));
-        airportFlights();
-        // stack3();
+        // parkingLot();                                        // question 1
+        // floodFill();                                         // question 2
+        // identifierCount("source.java");                      // question 3
+        // System.out.println(nestedTags("nestedTags.txt"));    // question 4
+        // airportFlights;                                      // question 5
+        //
     }
 
-    public void parkingLot()
+    public void parkingLot()                        // question 1
     {
-        Scanner keyboard = new Scanner(System.in);
-        Deque<Integer> parkingLot = new ArrayDeque<>();
+        Scanner keyboard = new Scanner(System.in);      // get keyboard input
+        Deque<Integer> parkingLot = new ArrayDeque<>(); // array decks
         Deque<Integer> street = new ArrayDeque<>();
-        int action = 1;
+        int action = 1;                                 // vehicle id variable
 
-        while (action != 0)
+        while (action != 0)                             // while input != 0
         {
             System.out.print("Enter car number: ");
-            action = keyboard.nextInt();
+            action = keyboard.nextInt();                    // get input
 
-            if (action > 0)
+            if (action > 0)                                 // if adding a car
             {
-                parkingLot.push(action);
-                System.out.println(street.toString());
+                parkingLot.push(action);                        // add the car
+                System.out.println(street.toString());          // print parking lot and street
                 System.out.println(parkingLot.toString());
                 System.out.println();
-            }
+            }                                               // else if removing a car that is parked
             else if (action < 0 && parkingLot.contains(-action))
-            {
+            {                                                   // while the next car in line isn't the requested car
                 while (parkingLot.peek() != -action && !parkingLot.isEmpty())
                 {
-                    street.push(parkingLot.pop());
-                    System.out.println(street.toString());
+                    street.push(parkingLot.pop());                  // move the car to the street
+                    System.out.println(street.toString());          // print parking lot and street
                     System.out.println(parkingLot.toString());
                     System.out.println();
-                }
-                parkingLot.pop();
-                System.out.println(street.toString());
+                }                                               // afterwards (once the next car is the requested one)
+                parkingLot.pop();                               // remove the car
+                System.out.println(street.toString());          // print parking lot and street
                 System.out.println(parkingLot.toString());
                 System.out.println();
-                while (!street.isEmpty())
+                while (!street.isEmpty())                       // until the street is empty
                 {
-                    parkingLot.push(street.pop());
-                    System.out.println(street.toString());
+                    parkingLot.push(street.pop());                  // park the next car back
+                    System.out.println(street.toString());          // print the parking lot and the street
                     System.out.println(parkingLot.toString());
                     System.out.println();
                 }
@@ -79,14 +79,14 @@ public class App
         }
     }
 
-    public void floodFill()
+    public void floodFill()                         // question 2
     {
-        Scanner keyboard = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);      // get keyboard input
         Deque<Coordinates> coordinates = new ArrayDeque<>();
+                                                        // create a coordinate stack
+        int[][] map = new int[10][10];                  // create a 10*10 map of 0s
 
-        int[][] map = new int[10][10];
-
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++)                    // print the map
         {
             for (int j = 0; j < 10; j++)
             {
@@ -95,20 +95,20 @@ public class App
             System.out.println();
         }
         System.out.println();
-
+                                                        // get the starting coordinates
         System.out.println("Enter x and y coordinates: ");
         coordinates.push(new Coordinates(keyboard.nextInt(), keyboard.nextInt()));
-
-        int order = 1;
-        while (!coordinates.isEmpty())
+                                                        // put them on the stack
+        int order = 1;                                  // variable to count the placement order
+        while (!coordinates.isEmpty())                  // while there are coordinates to place
         {
-            Coordinates a = coordinates.pop();
-            if(map[a.x][a.y] == 0)
+            Coordinates a = coordinates.pop();              // get the next coordinate
+            if(map[a.x][a.y] == 0)                          // if the coordinates cell is empty
             {
-                map[a.x][a.y] = order;
-                order++;
+                map[a.x][a.y] = order;                          // fill it
+                order++;                                        // increment the tile count
 
-                if (a.y > 0)
+                if (a.y > 0)                                    // put all empty adjecent tiles on the stack
                 {
                     if (map[a.x][a.y - 1] == 0)
                     {
@@ -139,7 +139,7 @@ public class App
             }
         }
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++)                    // print the map
         {
             for (int j = 0; j < 10; j++)
             {
@@ -149,119 +149,116 @@ public class App
         }
         System.out.println();
     }
-
+                                                    // question 3
     public void identifierCount(String path) throws FileNotFoundException
-    {
+    {                                                   // get a hashmap
         Map<String, HashSet<Integer>> map = new TreeMap<>();
-        File file = new File(path);
-        Scanner input = new Scanner(file);
-        int line = 1;
+        File file = new File(path);                     // get the source file
+        Scanner input = new Scanner(file);              // read the file
+        int line = 1;                                   // line countdown
         String token;
 
-        while (input.hasNextLine())
-        {
+        while (input.hasNextLine())                     // while there are lines in the source
+        {                                                   // read the line with a delimiter
             Scanner in = new Scanner(input.nextLine()).useDelimiter("[^A-Za-z0-9_]+");
-            while(in.hasNext())
+            while(in.hasNext())                             // while the line has tokens
             {
-                token = in.next();
-                if (map.containsKey(token))
+                token = in.next();                              // read the next token
+                if (map.containsKey(token))                     // if the map already has that token
                 {
-                    map.get(token).add(line);
+                    map.get(token).add(line);                       // add the line index to it
                 }
-                else
+                else                                            // otherwise
                 {
-                    HashSet<Integer> set = new HashSet<>();
-                    set.add(line);
-                    map.put(token, set);
+                    HashSet<Integer> set = new HashSet<>();         // create a new hashset
+                    set.add(line);                                  // add the line index
+                    map.put(token, set);                            // store the token in the map
                 }
             }
-            line++;
+            line++;                                         // increment line count
         }
-        for (String a : map.keySet())
+        for (String a : map.keySet())                   // for each token
         {
-            System.out.println(a + ": " + map.get(a));
+            System.out.println(a + ": " + map.get(a));      // print the token and its indexes
         }
     }
-
+                                                    // question 4
     public boolean nestedTags(String path) throws FileNotFoundException
     {
-        File file = new File(path);
-        Scanner input = new Scanner(file);
-        Deque<String> tags = new ArrayDeque<>();
-        Deque<String> closingTags = new ArrayDeque<>();
-        String tag;
+        File file = new File(path);                     // get source file
+        Scanner input = new Scanner(file);              // read the file
+        Deque<String> tags = new ArrayDeque<>();        // create tags stack
+        Deque<String> closingTags = new ArrayDeque<>(); // create closing tags stack
+        String tag;                                     // temporary tag storage
 
-        while (input.hasNextLine())
+        while (input.hasNext())                         // while the file has text
         {
-            while (input.hasNext())
-            {
-                tag = input.next();
-                tags.push(tag.substring(1, tag.length() - 1));
-            }
+            tag = input.next();                             // get the next tag
+            tags.push(tag.substring(1, tag.length() - 1));  // store it without brackets
         }
 
-        while(!tags.isEmpty())
+        while(!tags.isEmpty())                          // while there are tags to be processed
         {
-            tag = tags.pop();
-            if (tag.charAt(0) == '/')
+            tag = tags.pop();                               // take the next tag
+            if (tag.charAt(0) == '/')                       // if it's a closing tag
             {
-                closingTags.push(tag);
-            }
+                closingTags.push(tag);                          // put it into closing tags
+            }                                               // othewise if it doesn't have a matching closing tag
             else if (closingTags.isEmpty() || !tag.equals(closingTags.pop().substring(1)))
             {
-                return false;
+                return false;                                   // return false
             }
         }
-        return true;
+        return true;                                    // return true
     }
 
-    public void airportFlights()
+    public void airportFlights()                    // question 5
     {
-        Scanner keyboard = new Scanner(System.in);
-        Deque<String> takeoff = new ArrayDeque<>();
+        Scanner keyboard = new Scanner(System.in);      // get keyboard input
+        Deque<String> takeoff = new ArrayDeque<>();     // create inbound and outbound queues
         Deque<String> landing = new ArrayDeque<>();
         String input;
         String flight = "";
 
-        do
+        do                                              // do
         {
-            System.out.println("Takeoff: " + takeoff);
+            System.out.println("Takeoff: " + takeoff);      // print out both queues and ask for input
             System.out.println("Landing: " + landing);
             System.out.print("Enter command: ");
-            input = keyboard.nextLine();
-            if (input.contains(" "))
-            {
+            input = keyboard.nextLine();                    // get input
+            if (input.contains(" "))                        // if the input is multiple words
+            {                                                   // split input into command and flight ID
                 flight = input.substring(input.indexOf(" ") + 1);
                 input = input.substring(0, input.indexOf(" "));
             }
-            switch (input)
+            switch (input)                                  // switch over input
             {
                 case "takeoff" ->
-                {
-                    takeoff.add(flight);
+                {                                               // case takeoff
+                    takeoff.add(flight);                            // add the ID to the takeoff queue
                 }
                 case "land" ->
-                {
-                    landing.add(flight);
+                {                                               // case land
+                    landing.add(flight);                            // add the ID to the landing queue
                 }
                 case "next" ->
-                {
-                    if(!landing.isEmpty())
+                {                                               // case next
+                    if(!landing.isEmpty())                          // if there are planes to land
                     {
-                        System.out.println(landing.pop());
+                        System.out.println(landing.pop());              // remove the next plane
                     }
-                    else if (!takeoff.isEmpty())
+                    else if (!takeoff.isEmpty())                    // else if there are planes to take off
                     {
-                        System.out.println(takeoff.pop());
+                        System.out.println(takeoff.pop());              // remove the next plane
                     }
-                    else
-                    {
+                    else                                            // otherwise
+                    {                                                   // print message
                         System.out.println("No inbound or outbound flights.");
                     }
                 }
             }
-            System.out.println();
-        } while (!input.equals("quit"));
+            System.out.println();                           // separate line
+        } while (!input.equals("quit"));                // break if the input is quit
     }
 
     public void stack2()
